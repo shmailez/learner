9520babff5d1e12fb818ed20fa166a4602024881
Anton Mamontov <a.mamontov@p-s.kz>
2023-11-28 23:23:27 +0600

    routerdom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+import BasePage from "./layout/BasePage";
 
 function App() {
-  return <div className="App">NOT HEADER</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<BasePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/layout/BasePage.tsx
@@ -0,0 +1,5 @@
+const BasePage = () => {
+  return <>basepage</>;
+};
+
+export default BasePage;
