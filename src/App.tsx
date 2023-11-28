39233ac8601fae31f1aec913e0a3e49131b2b7d3
Anton Mamontov <a.mamontov@p-s.kz>
2023-11-28 23:25:39 +0600

    change base path

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<BasePage />} />
+        <Route path="/learner/" element={<BasePage />} />
       </Routes>
     </div>
   );
